3e3cd34ceec5d11299b35c4882fac26547390793
Piotr Gankiewicz <spetz90@gmail.com>
2017-10-02 12:31:29 +0200

    SignalR

--- scripts/dotnet-restore.sh
+++ scripts/dotnet-restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPOSITORIES=(Collectively.Api Collectively.Common Collectively.Messages Collectively.Services.Groups Collectively.Services.Mailing Collectively.Services.Medium Collectively.Services.Operations Collectively.Services.Statistics Collectively.Services.Remarks Collectively.Services.Storage Collectively.Services.Supervisor Collectively.Services.Users)
+REPOSITORIES=(Collectively.Api Collectively.Common Collectively.Messages Collectively.Services.Groups Collectively.Services.Mailing Collectively.Services.Medium Collectively.Services.Operations Collectively.Services.SignalR Collectively.Services.Statistics Collectively.Services.Remarks Collectively.Services.Storage Collectively.Services.Supervisor Collectively.Services.Users)
 for REPOSITORY in ${REPOSITORIES[*]}
 do
 	 echo ========================================================
